72510b0f63a207fc6cba90f8ec63da79d77faec4
Yourstepdad <52470778+Yourstepdad@users.noreply.github.com>
2019-07-11 11:38:37 -0700

    commmit project update 7-11-19

---
+++ Joshua_summer_1_project/js/Third_game.js
@@ -0,0 +1,390 @@
+function myFunction1() {
+  var x = document.getElementById("Term1");
+  var y = document.getElementById("Definition1");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("Term2");
+  var y = document.getElementById("Definition2");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction3() {
+  var x = document.getElementById("Term3");
+  var y = document.getElementById("Definition3");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction4() {
+  var x = document.getElementById("Term4");
+  var y = document.getElementById("Definition4");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction5() {
+  var x = document.getElementById("Term5");
+  var y = document.getElementById("Definition5");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction6() {
+  var x = document.getElementById("Term6");
+  var y = document.getElementById("Definition6");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction7() {
+  var x = document.getElementById("Term7");
+  var y = document.getElementById("Definition7");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction8() {
+  var x = document.getElementById("Term8");
+  var y = document.getElementById("Definition8");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction9() {
+  var x = document.getElementById("Term9");
+  var y = document.getElementById("Definition9");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction10() {
+  var x = document.getElementById("Term10");
+  var y = document.getElementById("Definition10");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction11() {
+  var x = document.getElementById("Term11");
+  var y = document.getElementById("Definition11");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction12() {
+  var x = document.getElementById("Term12");
+  var y = document.getElementById("Definition12");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction13() {
+  var x = document.getElementById("Term13");
+  var y = document.getElementById("Definition13");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction14() {
+  var x = document.getElementById("Term14");
+  var y = document.getElementById("Definition14");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction15() {
+  var x = document.getElementById("Term15");
+  var y = document.getElementById("Definition15");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction16() {
+  var x = document.getElementById("Term16");
+  var y = document.getElementById("Definition16");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction17() {
+  var x = document.getElementById("Term17");
+  var y = document.getElementById("Definition17");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction18() {
+  var x = document.getElementById("Term18");
+  var y = document.getElementById("Definition18");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction19() {
+  var x = document.getElementById("Term19");
+  var y = document.getElementById("Definition19");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction20() {
+  var x = document.getElementById("Term20");
+  var y = document.getElementById("Definition20");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction21() {
+  var x = document.getElementById("Term21");
+  var y = document.getElementById("Definition21");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction22() {
+  var x = document.getElementById("Term22");
+  var y = document.getElementById("Definition22");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction23() {
+  var x = document.getElementById("Term23");
+  var y = document.getElementById("Definition23");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction24() {
+  var x = document.getElementById("Term24");
+  var y = document.getElementById("Definition24");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction25() {
+  var x = document.getElementById("Term25");
+  var y = document.getElementById("Definition25");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction26() {
+  var x = document.getElementById("Term26");
+  var y = document.getElementById("Definition26");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction27() {
+  var x = document.getElementById("Term27");
+  var y = document.getElementById("Definition27");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction28() {
+  var x = document.getElementById("Term28");
+  var y = document.getElementById("Definition28");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction29() {
+  var x = document.getElementById("Term29");
+  var y = document.getElementById("Definition29");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function myFunction30() {
+  var x = document.getElementById("Term30");
+  var y = document.getElementById("Definition30");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
